b39e9aea4a875b1a0928ce82ff858e05a10020d7
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-10-11 14:32:23 +0100

    Game tracks the number of C and D items and update the total score with each

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -29,10 +29,16 @@ describe('Game', function() {
     expect(game.score).toEqual(15);
   });
 
-  xit('tracks the number of C items and adds 20 points to the score for each of them', function() {
+  it('tracks the number of C items and adds 20 points to the score for each of them', function() {
+    for(n=0;n<9;n++) { game.add_item('C'); }
+    expect(game.score).toEqual(180);
+    expect(game.c_items).toEqual(9);
   });
 
-  xit('tracks the number of D items and adds 15 points to the score for each of them', function() {
+  it('tracks the number of D items and adds 15 points to the score for each of them', function() {
+    for(n=0;n<11;n++) { game.add_item('D'); }
+    expect(game.score).toEqual(165);
+    expect(game.d_items).toEqual(11);
   });
 
   xit('knows that there is a bonus on A and adds 200 points for 3 A items', function() {
--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,22 @@
 function Game() {
   this.score = 0;
+  this.c_items = 0;
+  this.d_items = 0;
 }
 
 Game.prototype.add_item = function(item) {
   if (item === 'A') { this.score = this.score + 50 }
   else if (item === 'B') { this.score = this.score + 30 }
-  else if (item === 'C') { this.score = this.score + 20 }
-  else if (item === 'D') { this.score = this.score + 15 };
+  else if (item === 'C') { this.add_C_items(); }
+  else if (item === 'D') { this.add_D_items();};
+};
+
+Game.prototype.add_C_items = function() {
+  this.c_items ++; 
+  this.score = this.score + 20;
+};
+
+Game.prototype.add_D_items = function() {
+  this.d_items ++; 
+  this.score = this.score + 15;
 };
