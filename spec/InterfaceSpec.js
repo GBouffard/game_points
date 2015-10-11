6e9c4328ef1adc1527395ed1fe09e512211c6732
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-10-11 19:46:17 +0100

    interface has no collected items in the list & 0 as score and bonus when initialised

--- spec/InterfaceSpec.js
+++ spec/InterfaceSpec.js
@@ -6,7 +6,12 @@ describe('Interface', function() {
     $.holdReady(false);
   });
 
-  xit('has no collected items in the list when initialised', function(){
+  it('has no collected items in the list & 0 as score and bonus when initialised', function(){
+      expect("#items_box").toBeEmpty();
+      expect("#quantities_box").toBeEmpty();
+      expect("#scores_box").toBeEmpty();
+      expect("#total_bonus").toHaveText(0);
+      expect("#total_score").toHaveText(0);
   });
 
   xit('outputs A in the player s items list (as well as quantity 1) when item A is clicked once', function(){
@@ -15,7 +20,10 @@ describe('Interface', function() {
   xit('updates score and quantities of the A items in the list when A is clicked multiple times', function(){
   });
 
-  xit('outputs B, C and D in the player s items list (as well as quantities and scores) when clicked', function(){
+  xit('updates total score and total bonus when A is clicked multiple times', function(){
+  });
+
+  xit('outputs B, C and D in the player s items list (as well as well as their quantities and scores) when clicked', function(){
   });
 
   xit('updates the total score as items are being added', function(){
