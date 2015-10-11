eb8eea83b8037ac188a07aa52af714faf7b730bd
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-10-11 18:47:25 +0100

    Interface, JQuery and Node Sever set-ups

---
+++ spec/InterfaceSpec.js
@@ -0,0 +1,11 @@
+describe('Interface', function() {
+
+  beforeEach(function() {
+    jasmine.getFixtures().fixturesPath = '.';
+    loadFixtures('index.html');
+    $.holdReady(false);
+  });
+
+  xit('has no tests yet', function(){
+  });
+}); 
